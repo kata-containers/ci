3650a6e53de58b027f735d997b605d9a09006070
Graham Whaley <graham.whaley@intel.com>
2018-09-05 14:41:36 +0100

    VMs: metrics: add jenkins node helper scripts
    When setting up the Jenkins slave node SSH config, it is helpful
    to have a couple of scripts to aid in the startup and shutdown
    of the VM with the agent in it. Add those scripts here so they
    don't need to be re-created by each end user.
    
    Signed-off-by: Graham Whaley <graham.whaley@intel.com>

---
+++ VMs/metrics/jenkins_stop.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Copyright (c) 2018 Intel Corporation
+# 
+# SPDX-License-Identifier: Apache-2.0
+
+ROOTDIR=${HOME}/kata-containers-ci/VMs/metrics
+# Ensure virsh can see the correct domain for our VMs.
+# On some systems a Jenkins ssh connection will connect to
+# qemu:///session by default, and thus will fail to clone/run
+# a new VM.
+export LIBVIRT_DEFAULT_URI=qemu:///system
+
+echo "In $0"
+
+if [ $# -ne 1 ]; then
+	echo "Require VM config name as only parameter"
+	exit 1
+fi
+
+cd ${ROOTDIR}
+./7_delete_clone.sh $1
+
+echo "Done $0"
+
